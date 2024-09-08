dd6dab31b0b78271f2d154d13ef47097eedee263
Vinícius de Paula <manoseilacara123@gmail.com>
2024-09-08 10:27:19 -0300

    Primeira classe: Position

---
+++ src/application/Main.java
@@ -0,0 +1,10 @@
+package application;
+
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+        Position position = new Position(5, 3);
+        System.out.println(position);
+    }
+}
