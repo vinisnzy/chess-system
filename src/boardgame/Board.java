514f2c60da62f4cf861a883d91e47284c0a5f4b2
Vinícius de Paula <manoseilacara123@gmail.com>
2024-09-08 12:04:38 -0300

    Colocando peças no tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -34,4 +34,9 @@ public class Board {
     public Piece piece(Position pos){
         return pieces[pos.getRow()][pos.getColumn()];
     }
+
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
 }
